369de2c6d4a9b313e4d03bb154f261ed53c4f303
Serhii Poprovka <s.poprovka@upstars.com>
2025-03-05 18:43:44 +0200

    init

--- src/permission_exchanger/permission_gateway/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-# from src.permission_exchanger.permission_gateway.views import receive_all_permissions
-#
-# urlpatterns = [
-# 	path('adopt_permissions/', receive_all_permissions, name='receive_all_permissions_from_child'),
-# ]

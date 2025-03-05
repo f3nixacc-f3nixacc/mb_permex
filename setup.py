6dcb678875f15eb0c99859edb3ad27c2e40ab02b
Serhii Poprovka <s.poprovka@upstars.com>
2025-03-05 18:35:08 +0200

    init

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
 	name='permission_exchanger',
     version='1.0.1',
     author="Serhii Poprovka",
-    packages=find_packages('src'),
+    packages=find_packages('src', include=["src.permission_exchanger", "src.permission_exchanger.*"]),
     package_dir={'': 'src'},
 )

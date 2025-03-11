51e92eff39f369bc04902f772219e3fc8ed5c2aa
Serhii Poprovka <s.poprovka@upstars.com>
2025-03-11 17:56:36 +0200

    perms

--- permission_gateway/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from unfold.admin import ModelAdmin
-from permission_gateway.models import UserPlatformPermission
-
-
-@admin.register(UserPlatformPermission)
-class UserPlatformPermissionAdmin(ModelAdmin):
-	pass

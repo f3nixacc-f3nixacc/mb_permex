dac74f8c133e870c6e679cb0da6060f7195d65ec
Serhii Poprovka <s.poprovka@upstars.com>
2025-03-12 17:03:44 +0200

    +

--- permission_client/views.py
+++ permission_client/views.py
@@ -35,4 +35,10 @@ def receive_all_permissions(request):
 @require_POST
 @csrf_exempt
 def receive_all_teams(request):
+	with open("test.txt", "a") as f:
+		f.write(str(request.data))
+		f.write("\n")
+		f.write(str(request.POST))
+		f.write("\n")
+		f.close()
 	return JsonResponse({"request.POST":request.POST, "request.data":request.data})

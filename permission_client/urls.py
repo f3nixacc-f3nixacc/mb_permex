ad64ab0ffc92c0594613e1de7351b18835e39556
Serhii Poprovka <s.poprovka@upstars.com>
2025-03-12 17:53:42 +0200

    +

--- permission_client/urls.py
+++ permission_client/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from permission_client.views import get_all_permissions, receive_all_permissions, receive_team
+from permission_client.views import get_all_permissions, receive_all_permissions, receive_team, receive_teams
 
 urlpatterns = [
 	path('permissions/', get_all_permissions, name='get_all_permissions_from_child'),
 	path('get_permissions/', receive_all_permissions, name='receive_permissions_from_host'),
 	path('team/', receive_team, name='receive_single_team_from_host'),
+	path('teams/', receive_teams, name='receive_all_teams_from_host'),
 ]

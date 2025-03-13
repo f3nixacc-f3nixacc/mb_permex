cb2d1b155a2b8aa04822fef5473ef80e78bdb11f
Serhii Poprovka <s.poprovka@upstars.com>
2025-03-13 14:08:39 +0200

    +

--- permission_client/urls.py
+++ permission_client/urls.py
@@ -11,7 +11,7 @@ from permission_client.views import (
 urlpatterns = [
 	path('permissions/', get_all_permissions, name='get_all_permissions_from_child'),
 	path('get_permissions/', receive_all_permissions, name='receive_permissions_from_host'),
+	path('team/permissions', receive_team_permissions, name='receive_team_permissions_from_host'),
 	path('team/', receive_team, name='receive_single_team_from_host'),
 	path('teams/', receive_teams, name='receive_all_teams_from_host'),
-	path('team/permissions', receive_team_permissions, name='receive_team_permissions_from_host'),
 ]

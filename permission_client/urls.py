ad7619c96f08456a35cde324319071c655cff2c3
Serhii Poprovka <s.poprovka@upstars.com>
2025-04-01 17:52:57 +0300

    +

--- permission_client/urls.py
+++ permission_client/urls.py
@@ -5,7 +5,8 @@ from permission_client.views import (
 	receive_all_permissions,
 	receive_permission_group,
 	receive_permission_groups,
-	receive_permission_group_permissions
+	receive_permission_group_permissions,
+	get_user_groups
 )
 
 urlpatterns = [
@@ -14,4 +15,5 @@ urlpatterns = [
 	path('permission_group/', receive_permission_group, name='receive_single_permission_group_from_host'),
 	path('permission_groups/', receive_permission_groups, name='receive_all_permission_groups_from_host'),
 	path('permission_group/permissions/', receive_permission_group_permissions, name='receive_permission_group_permissions_from_host'),
+	path('user_groups/', get_user_groups, name='get_user_groups'),
 ]

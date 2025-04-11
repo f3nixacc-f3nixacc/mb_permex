af2c806d5ee2d71a6a8ede7715cb161cf72d6951
Serhii Poprovka <s.poprovka@upstars.com>
2025-04-11 16:54:56 +0300

    sync_selected_teams

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='permission_exchanger',
-    version='1.0.5',
+    version='1.0.2',
     author="Serhii Poprovka",
     packages=find_packages(),
 	include_package_data=True,
